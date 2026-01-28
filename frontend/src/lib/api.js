5d70b5365f09e1c6be6978bfda9b9942233b16d5
Tanmay9724349 <156205714+Tanmay9724349@users.noreply.github.com>
2026-01-28 18:01:46 +0000

    Add api.js

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,3 +1,3 @@
-const API_URL = import.meta.env.VITE_API_URL || "https://auction-website-98pd.onrender.com";
+const API_URL =  "https://auction-website-98pd.onrender.com";
 
 export default API_URL;
