04d36e65b387f737832a9f2d064c810debb3e820
Tanmay9724349 <156205714+Tanmay9724349@users.noreply.github.com>
2026-01-31 10:10:39 +0530

    Update auth.js

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -4,13 +4,12 @@ import ErrorHandler from "./error.js";
 import { catchAsyncErrors } from "../middlewares/catchAsyncErrors.js";
 
 export const isAuthenticated = catchAsyncErrors(async (req, res, next) => {
-  // const token = req.cookies.token;
-  // if (!token) {
-  //   return next(new ErrorHandler("User not authenticated.", 400));
-  // }
-  // const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
-  // req.user = await User.findById(decoded.id);
-  req.user = await User.findById("6783c435b5cd21581c881029"); // Temporary hardcoded user for testing
+  const token = req.cookies.token;
+  if (!token) {
+    return next(new ErrorHandler("User not authenticated.", 400));
+  }
+  const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+  req.user = await User.findById(decoded.id);
   next();
 });
 
