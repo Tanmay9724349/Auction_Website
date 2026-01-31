98a3bcfbaf4f64fe80f423919acdd088dcf91086
Tanmay9724349 <156205714+Tanmay9724349@users.noreply.github.com>
2026-01-31 10:07:09 +0530

    Add hardcoded user for testing in auth.js
    Added temporary hardcoded user for testing in auth middleware.

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -10,6 +10,7 @@ export const isAuthenticated = catchAsyncErrors(async (req, res, next) => {
   // }
   // const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
   // req.user = await User.findById(decoded.id);
+  req.user = await User.findById("6783c435b5cd21581c881029"); // Temporary hardcoded user for testing
   next();
 });
 
